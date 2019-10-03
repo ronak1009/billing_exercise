18b482e280753cd4226db5aa8b802e212f5c9120
Ronak Shah <ronak1009@gmail.com>
2019-10-03 22:12:48 +0530

    initiating prroject

---
+++ billing/billing/main.cpp
@@ -0,0 +1,12 @@
+// author: Ronak SHAH
+// Filename: billing.cpp 
+// Description: contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Initiating project";
+}
+
